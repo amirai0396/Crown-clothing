5f59e62ca795da009a7486663e99698be35a97de
Amritanshu rai <amritanshurai13@gmail.com>
2021-03-01 02:33:13 +0530

    added header&links

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import './App.css';
+import Header from './component/header/header.component';
 
 import Homepage from './pages/homepage/homepage.component';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         ̥<Route exact path='/' component={Homepage} />
         ̥<Route path='/shop' component={ShopPage} />
