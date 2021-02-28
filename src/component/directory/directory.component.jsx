7c47e5103f237847c89855de6acd8f3c5bc73038
Amritanshu rai <amritanshurai13@gmail.com>
2021-02-28 20:57:52 +0530

    updated our menu-Items to navigate withRouter HOC

--- src/component/directory/directory.component.jsx
+++ src/component/directory/directory.component.jsx
@@ -11,28 +11,33 @@ class Directory extends React.Component {
           title: 'Hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
           id: 1,
+          linkUrl: 'Hats',
         },
         {
           title: 'Jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
           id: 2,
+          linkUrl: '',
         },
         {
           title: 'Sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
           id: 3,
+          linkUrl: '',
         },
         {
           title: 'Womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           size: 'large',
           id: 4,
+          linkUrl: '',
         },
         {
           title: 'Mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           size: 'large',
           id: 5,
+          linkUrl: '',
         },
       ],
     };
@@ -40,8 +45,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className='directory-menu '>
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionsProps }) => (
+          <MenuItem key={id} {...otherSectionsProps} /> // using the spread operator we can pass all the section props like this {...spreadoperator} but id we cant because id is not passed as Id it's passed as key{}
         ))}
       </div>
     );
