c1be9f5e606ee7c83e1528bc7d42a6f6e820dba5
Amritanshu rai <amritanshurai13@gmail.com>
2021-03-09 00:13:24 +0530

    added redux-persist in the store

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import store from './component/redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
+import { store, persistor } from './component/redux/store';
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
